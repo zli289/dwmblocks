f03a2ddb5a7af2b270b60fb568a1300af497a491
Zequn <zli289@gwu.edu>
2020-07-04 03:40:10 -0400

    config changes

--- blocks.h
+++ blocks.h
@@ -1,20 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"", "~/bin/statusbar/clock",						5,		0},
+	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
+	{"Volumn:", 	"~/scripts/statusbar/volumn",		0,		9},
+	{"Bright:", 	"~/scripts/statusbar/brightness",	0,		10},
+	{"Battery:", 	"~/scripts/statusbar/battery",		5,		3},
+	{"", 		"~/scripts/statusbar/clock",		2,		1},
+//	{"", "~/bin/statusbar/battery",						5,		0},
+//	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+//	{"", "~/bin/statusbar/clock",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
